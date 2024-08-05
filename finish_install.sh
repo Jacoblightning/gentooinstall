eb4bb3ff3b5835d91d214c2d7122dc6a7f18bc08
Jacob <t00niyf5@duck.com>
2024-08-05 17:22:08 -0400

    Add files via upload

---
+++ finish_install.sh
@@ -0,0 +1,162 @@
+#!/bin/bash
+
+source /etc/profile 
+
+runningon=$1
+
+if [[ "$runningon" == *"nvme"* ]]; then
+    drivepref="${runningon}p"
+else
+    drivepref="${runningon}"
+fi
+
+echo "Creating boot directories"
+if [ -d /sys/firmware/efi ]; then
+    mkdir /efi 
+    mount "${drivepref}1" /efi
+else
+    mount "${drivepref}1" /boot
+fi
+
+echo "Syncing repos"
+emerge-webrsync
+
+echo "Installing mirrorselect tool"
+emerge --verbose --oneshot app-portage/mirrorselect
+
+read -p "Press enter to select your mirrors"
+mirrorselect -i -o >> /etc/portage/make.conf
+
+echo "Re-syncing mirrors"
+emerge --sync
+
+read -p "Select a profile if you want. Press enter to view the list: "
+eselect profile list
+read -p "Select a profile or 0 to keep it as is: " prof
+
+if [ $prof != '0' ]; then
+    eselect profile set $prof
+fi
+
+read -p "Would you like binary packages? (Y/N): " confirm
+if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
+    mkdir --parents /etc/portage/binrepos.conf
+    echo "[binhost]" > /etc/portage/binrepos.conf
+    echo "priority = 9999" >> /etc/portage/binrepos.conf
+    
+    echo "Calculating current version"
+    version=$(curl "https://distfiles.gentoo.org/releases/amd64/binpackages/" | grep -oE '[0-9]{2,}\.[0-9]+' | sed 1q)
+    
+    read -p "Would you like hardened binary packages? (Y/N): " confirm
+    if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
+        echo "sync-uri = https://distfiles.gentoo.org/releases/amd64/binpackages/${version}/x86-64_hardened/" >> /etc/portage/binrepos.conf
+    else
+        echo "sync-uri = https://distfiles.gentoo.org/releases/amd64/binpackages/${version}/x86-64/" >> /etc/portage/binrepos.conf
+    fi
+    
+    echo -e '\n\n# Appending getbinpkg to the list of values within the FEATURES variable' >> /etc/portage/make.conf
+    echo 'FEATURES="${FEATURES} getbinpkg"' >> /etc/portage/make.conf
+    echo "# Require signatures" >> /etc/portage/make.conf
+    echo 'FEATURES="${FEATURES} binpkg-request-signature"' >> /etc/portage/make.conf
+    
+    echo "Recompiling keyring... Please wait"
+    getuto
+fi
+
+
+echo "Configuring CPU flags"
+emerge --oneshot app-portage/cpuid2cpuflags
+echo "*/* $(cpuid2cpuflags)" > /etc/portage/package.use/00cpu-flags
+
+echo "Setting up licenses"
+
+echo -e "\n\n# Overrides the profile's ACCEPT_LICENSE default value" >> /etc/portage/make.conf
+echo 'ACCEPT_LICENSE="-* @FREE @BINARY-REDISTRIBUTABLE"' >> /etc/portage/make.conf
+
+echo "app-arch/unrar unRAR" > /etc/portage/package.license/kernel
+echo "sys-kernel/linux-firmware linux-fw-redistributable" >> /etc/portage/package.license/kernel
+echo "sys-firmware/intel-microcode intel-ucode" >> /etc/portage/package.license/kernel
+
+echo "Performing system update"
+
+emerge --verbose --update --deep --newuse @world
+
+echo "Removing old packages"
+
+emerge --depclean
+
+if which systemctl; then
+    echo "Systemd users, this script ends here. Please continue to manually finish from https://wiki.gentoo.org/wiki/Handbook:AMD64/Installation/Base#Optional:_Using_systemd_as_the_system_and_service_manager"
+    exit
+fi
+
+echo "Generating locales"
+locale-gen
+
+echo "Spawning shell so you can set your timezone as specified in https://wiki.gentoo.org/wiki/Handbook:AMD64/Installation/Base#Timezone"
+echo "Type exit when done"
+bash
+
+echo "Setting new timezone config"
+emerge --config sys-libs/timezone-data
+
+echo 'LC_COLLATE="C.UTF-8"' >> /etc/env.d/02locale
+echo 'LANG="en_US.UTF-8"' >> /etc/env.d/02locale
+
+env-update && source /etc/profile 
+
+echo "Installing additional firmware"
+
+emerge sys-kernel/linux-firmware
+emerge sys-firmware/sof-firmware
+if lscpu | grep -i intel > /dev/null; then
+    emerge sys-firmware/intel-microcode
+fi
+
+echo "Installing the kernel (binary)"
+echo "sys-kernel/installkernel dracut" >> /etc/portage/package.use/installkernel
+
+emerge sys-kernel/gentoo-kernel-bin
+
+echo "Creating fstab"
+if [ -d /sys/firmware/efi ]; then
+    echo "${drivepref}1 /efi vfat defaults 0 2" > /etc/fstab
+else
+    echo "${drivepref}1 /boot xfs defaults 0 2" > /etc/fstab
+fi
+echo "${drivepref}2 none swap sw 0 0
+${drivepref}3 / xfs defaults,noatime 0 1
+
+/dev/cdrom /mnt/cdrom auto noauto,user 0 0" >> /etc/fstab
+
+read -p "Enter your new hostname: " hname
+echo "$hname" > /etc/hostname
+
+echo "Configuring network"
+emerge net-misc/dhcpcd
+rc-update add dhcpcd default 
+rc-service dhcpcd start
+
+echo "127.0.0.1 ${hname} localhost ${hname}.local" >> /etc/hosts
+
+clear
+echo "Please set the root password"
+passwd
+
+echo "Installing additional tools..."
+emerge app-admin/sysklogd sys-fs/xfsprogs sys-fs/e2fsprogs sys-fs/dosfstools sys-process/cronie sys-block/io-scheduler-udev-rules sys-apps/mlocate app-shells/bash-completion net-misc/chrony net-wireless/iw net-wireless/wpa_supplicant
+rc-update add cronie default
+rc-update add sysklogd default
+rc-update add chronyd default
+
+echo "Installing the bootloader..."
+if [ -d /sys/firmware/efi ]; then
+    echo 'GRUB_PLATFORMS="efi-64"' >> /etc/portage/make.conf
+fi
+emerge --verbose sys-boot/grub
+if [ -d /sys/firmware/efi ]; then
+    grub-install --efi-directory=/efi
+else
+    grub-install $runningon
+fi
+grub-mkconfig -o /boot/grub/grub.cfg
---
+++ gentooinstall.sh
@@ -0,0 +1,253 @@
+#!/bin/bash
+
+echo "Starting tests..."
+
+echo "Confirming architecture..."
+if [ $(uname -m) != 'x86_64' ]; then
+    echo "This script only works on amd64"
+    exit
+fi
+
+echo "Testing internet..."
+if ping -c 3 1.1.1.1 > /dev/null 2>&1; then
+    echo "Internet works"
+else
+    echo "Internet does not work. Please fix this and rerun the script"
+    exit
+fi
+
+echo "Testing DNS..."
+
+if curl --location gentoo.org --output /dev/null > /dev/null 2>&1; then
+    echo "DNS Works"
+else
+    echo "DNS does not work. Please fix this and rerun the script"
+    exit
+fi
+
+echo "Starting Gentoo Install..."
+
+echo "Please enter the path of the disk to install on. Options:"
+lsblk -do PATH,SIZE -e7 -e 254
+read -p "Is your device listed? (Y/N): " confirm
+if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
+    read -p "PLease enter the path of the device to install on: " installto
+    if [ -e "$installto" ] && [ ! -f "$installto" ] && [ ! -d "$installto" ]; then echo; else
+        echo "Device not found. Please re-run script."
+        exit
+    fi
+    read -p "This device will be erased. Ok? (Y/N): " confirm
+    if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
+        echo "Great."
+    else
+        echo "Please re-run script to select another device."
+        exit
+    fi
+else
+    echo "Sorry, please plug in the device and re-run the script."
+    exit 1
+fi
+
+if [[ "$installto" == *"nvme"* ]]; then
+    drivepref="${installto}p"
+else
+    drivepref="${installto}"
+fi
+
+echo "Checking boot mode and creating filesystem..."
+if [ -d /sys/firmware/efi ]; then
+    echo "Booted in UEFI mode. Installing UEFI system."
+    curl "https://raw.githubusercontent.com/Jacoblightning/gentooinstall/main/uefi_gpt.txt" | sfdisk --wipe always "$installto"
+    mkfs.vfat -F 32 "${drivepref}1"
+    mkdir --parents /mnt/gentoo/efi
+else
+    echo "Booted in BIOS mode. Installing BIOS system."
+    curl "https://raw.githubusercontent.com/Jacoblightning/gentooinstall/main/bios_mbr.txt" | sfdisk --wipe always "$installto"
+    mkfs.xfs "${drivepref}1"
+    mkdir --parents /mnt/gentoo
+fi
+
+echo "Finalizing filesystem..."
+mkfs.xfs "${drivepref}3"
+mkswap "${drivepref}2"
+swapon "${drivepref}2"
+
+echo "Mounting system"
+mount "${drivepref}3" /mnt/gentoo
+
+cd /mnt/gentoo
+
+echo "Correcting system clock..."
+chronyd -q
+
+clear
+
+echo "First, Choose your init system."
+
+PS3="Select your system: "
+
+select initsystem in openrc systemd
+do
+    break
+done
+
+clear
+
+echo "${initsystem}, great choice. Now select your System Type"
+
+systypes=('server' 'desktop' 'server-hardened')
+select systype in "${systypes[@]}"
+do
+    break
+done
+
+clear
+
+mirrors=("gentoo.osuosl.org" "distfiles.gentoo.org")
+gentoopart="/releases/amd64/autobuilds"
+startpart="/current-stage3-amd64-"
+
+declare -A systems
+
+systems['openrc-desktop']="desktop-openrc"
+systems['systemd-desktop']="desktop-systemd"
+systems['openrc-server']="openrc"
+systems['systemd-server']="systemd"
+systems['openrc-server-hardened']="hardened-openrc"
+systems['systemd-server-hardened']="hardened-systemd"
+
+echo "Testing Mirrors..."
+
+for mirror in "${mirrors[@]}"; do
+    ping -c 3 "$mirror" && usemirror="$mirror" && break
+done
+
+if [ -z "$usemirror" ]; then
+    echo "All mirrors were down. Please enter the url of a working mirror."
+    read -p "Url: " usemirror
+    
+    echo "Checking mirror..."
+    
+    if ping -c 3 "$usemirror";then echo; else
+        echo "Mirror failed. Install the rest yourself (Too far in to restart script.)"
+    fi
+fi
+
+echo "Using mirror $usemirror"
+
+sysindex="${initsystem}-${systype}"
+
+url="https://${usemirror}${gentoopart}${startpart}${systems[$sysindex]}/"
+
+echo "Finding newest version"
+
+# Dumb magic
+filename=$(curl $url | grep -oE '"stage3-amd64.*?.tar.xz"' | grep --color=none -oE "s.*z")
+
+newest="${url}${filename}"
+
+echo "Newest file is $newest"
+
+wget "$newest"
+
+echo "Preparing to verify integrity"
+
+wget "${newest}.sha256"
+wget "${newest}.DIGESTS"
+wget "${newest}.asc"
+
+echo "Checking file integrity"
+
+if sha256sum --check "${filename}.sha256"; then
+    echo "Sha256 passed."
+else
+    echo "Sha256 failed. Exiting"
+    exit
+fi
+
+if cat "${filename}.DIGESTS" | grep --color=none $(openssl dgst -r -sha512 "$filename" | grep -o ".* "); then
+    echo "Sha512 passed."
+else
+    echo "Sha512 failed. Exiting"
+    exit
+fi
+
+if cat "${filename}.DIGESTS" | grep --color=none $(openssl dgst -r -blake2b512 "$filename" | grep -o ".* "); then
+    echo "Blake passed."
+else
+    echo "Blake failed. Exiting"
+    exit
+fi
+
+if [ -f /usr/share/openpgp-keys/gentoo-release.asc ];then
+    gpg --import /usr/share/openpgp-keys/gentoo-release.asc
+else
+    wget -O - https://qa-reports.gentoo.org/output/service-keys.gpg | gpg --import
+fi
+
+if gpg --verify "$filename".DIGESTS; then
+    echo "Digests are valid."
+else
+    echo "Invalid Digests. Exiting"
+    exit
+fi
+
+
+if gpg --verify "$filename".sha256; then
+    echo "sha256 is valid."
+else
+    echo "Invalid sha256. Exiting"
+    exit
+fi
+
+if gpg --verify "${filename}.asc"; then
+    echo "signature is valid!"
+else
+    echo "Invalid signature. Exiting"
+    exit
+fi
+
+sleep 1
+
+rm -v ${filename}.*
+
+echo "Extracting filesystem..."
+tar xpvf $filename --xattrs-include='*.*' --numeric-owner
+
+echo "Setting Flags"
+sed -i -e 's/COMMON_FLAGS="-O2 -pipe"/COMMON_FLAGS="-march=native -O2 -pipe"/g' etc/portage/make.conf
+
+echo "Preparing to chroot..."
+cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
+
+if which arch-chroot; then
+    croot=arch-chroot
+else
+    mount --types proc /proc /mnt/gentoo/proc
+    mount --rbind /sys /mnt/gentoo/sys
+    mount --make-rslave /mnt/gentoo/sys
+    mount --rbind /dev /mnt/gentoo/dev
+    mount --make-rslave /mnt/gentoo/dev
+    mount --bind /run /mnt/gentoo/run
+    mount --make-slave /mnt/gentoo/run 
+    
+    test -L /dev/shm && rm /dev/shm && mkdir /dev/shm
+    mount --types tmpfs --options nosuid,nodev,noexec shm /dev/shm 
+    
+    chmod 1777 /dev/shm /run/shm
+    
+    croot=chroot
+fi
+
+echo "Downloading and moving new script"
+wget "https://raw.githubusercontent.com/Jacoblightning/gentooinstall/main/finish_install.txt"
+
+echo "Runnins finish_install script"
+chroot /mnt/gentoo /bin/bash /finish_install.sh $installto
+
+umount -l /mnt/gentoo/dev{/shm,/pts,} 
+umount -R /mnt/gentoo 
+
+echo "Gentoo is Installed!!!"
+read -p "Press enter to reboot!!!"
+reboot
